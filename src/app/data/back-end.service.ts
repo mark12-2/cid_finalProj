efd2a958abc1754184faf1fdaea772f2696b2599
Mak <aelacita@gmail.com>
2023-12-04 11:40:22 +0800

    saving data in owner id and each post inside as array

--- src/app/data/back-end.service.ts
+++ src/app/data/back-end.service.ts
@@ -2,33 +2,58 @@ import { Injectable } from '@angular/core';
 import { PostService } from './post.service';
 import { Post } from '../post.model';
 import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs';
+import { map, take, tap } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AuthService } from '../auth/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackEndService {
 
-  constructor(private postService: PostService, private http: HttpClient, private afAuth: AngularFireAuth) { }
+  constructor(private postService: PostService, private http: HttpClient, private afAuth: AngularFireAuth, private authService: AuthService) { }
 
   //saving data from creating post
   saveData(post: Post){
-    this.http.post(`https://crud-app-f0d6e-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json`, post)
-    .subscribe((res)=>{
-      console.log(res);
-    })
+    this.authService.getUserId().then(userId => {
+        if (userId) {
+            // Fetch existing posts
+            this.http.get<Post[]>(`https://crud-app-f0d6e-default-rtdb.asia-southeast1.firebasedatabase.app/posts/${userId}.json`)
+            .pipe(take(1)) // Take only the first value from the Observable
+            .subscribe(existingPosts => {
+                let posts = existingPosts;
+                if (!posts) {
+                    posts = [];
+                }
+                // Add new post to the array
+                posts.push(post);
+                // Save updated array back to Firebase
+                this.http.put(`https://crud-app-f0d6e-default-rtdb.asia-southeast1.firebasedatabase.app/posts/${userId}.json`, posts)
+                .subscribe((res)=>{
+                    console.log(res);
+                });
+            });
+        } else {
+            console.error('User is not authenticated');
+        }
+    });
 }
 
   //fetching data from firebase for viewing in home
   fetchData(){
-    return this.http.get<{[key: string]: Post}>('https://crud-app-f0d6e-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
+    return this.http.get<{[key: string]: {[key: string]: Post}}>('https://crud-app-f0d6e-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
     .pipe(
         map(responseData => {
             const postArray: Post[] = [];
-            for (const key in responseData) {
-                if (responseData.hasOwnProperty(key)) {
-                    postArray.push(responseData[key]);
+            for (const userKey in responseData) {
+                if (responseData.hasOwnProperty(userKey)) {
+                    const userPosts = responseData[userKey];
+                    for (const postKey in userPosts) {
+                        if (userPosts.hasOwnProperty(postKey)) {
+                            const post = userPosts[postKey];
+                            postArray.push(post);
+                        }
+                    }
                 }
             }
             return postArray;
@@ -41,13 +66,12 @@ export class BackEndService {
 }
  
   //updating the data 
-  updateData(index: number, updatedPost: Post) {
-        this.postService.updatePost(index, updatedPost);
-    this.http.put(`https://crud-app-f0d6e-default-rtdb.asia-southeast1.firebasedatabase.app/posts/${index}.json`, updatedPost)
+  updateData(userId: string, postId: string, updatedPost: Post) {
+    this.http.put(`https://crud-app-f0d6e-default-rtdb.asia-southeast1.firebasedatabase.app/posts/${userId}/${postId}.json`, updatedPost)
         .subscribe(response => {
             console.log(response);
         });
-  }
+}
 
   //deleting data from firebase
   deleteData(index: number){
@@ -59,17 +83,19 @@ export class BackEndService {
 }
 
   //comment push to firebase 
-  addComment(index: number, comment: string) {
-    const post = this.postService.getSpecPost(index);
-    if (!post.comments) {
-        post.comments = [];
-    }
-    post.comments.push(comment);
-    this.http.put(`https://crud-app-f0d6e-default-rtdb.asia-southeast1.firebasedatabase.app/posts/${index}.json`, post)
-      .subscribe(response => {
-        console.log(response);
-      });
+  addComment(userId: string, postId: string, comment: string) {
+    this.postService.getSpecPost(userId, postId).subscribe(post => {
+        if (!post.comments) {
+            post.comments = [];
+        }
+        post.comments.push(comment);
+        this.http.put(`https://crud-app-f0d6e-default-rtdb.asia-southeast1.firebasedatabase.app/posts/${userId}/${postId}.json`, post)
+          .subscribe(response => {
+            console.log(response);
+          });
+    });
+}
+
 }
 
 
-}
